04512377b8aae8a08ddf467afcd9dc5e8b88fcd5
Nicholas Smith <ut101@hotmail.com>
2026-07-24 10:33:30 +0100

    Added logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/detective.svg";
 
 const Navbar = () => {
   return (
-    <div className="navbar flex items-center justify-between p-5  sticky top-0 z-50 bg-canvas/85 backdrop-blur border-b border-hairline">
-      <div className="logo">
-        <img src="" alt="Mr White Logo" />
-        Mr. White
+    <div className="navbar p-5 sticky top-0 z-50 bg-canvas/85 backdrop-blur border-b border-hairline">
+      <div className="wrapper flex items-center justify-between">
+        <Link to="/">
+          <div className="logo flex items-center gap-3">
+            <img src={logo} alt="Mr White Logo" width="40" />
+            Mr. White
+          </div>
+        </Link>
+        <nav className="flex gap-8 items-center">
+          <Link to="/" className="text-sm">
+            Home
+          </Link>
+          <Link to="/play" className="text-sm">
+            Play
+          </Link>
+          <Link to="/about" className="text-sm">
+            About
+          </Link>
+        </nav>
       </div>
-      <nav className="flex gap-2 items-center">
-        <Link to="/">Home</Link>
-        <Link to="/play">Play</Link>
-        <Link to="/about">About</Link>
-      </nav>
     </div>
   );
 };
